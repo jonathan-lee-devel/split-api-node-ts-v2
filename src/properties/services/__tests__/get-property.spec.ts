489216e43f97e65e75a1396c5c5f5f349d4d68f2
Jonathan Lee <jonathan.lee.devel@gmail.com>
2022-07-08 22:33:26 +0100

    Added tests to get-property.spec.ts

--- src/properties/services/__tests__/get-property.spec.ts
+++ src/properties/services/__tests__/get-property.spec.ts
@@ -43,4 +43,98 @@ describe('Get property', () => {
         expect(result.status).toStrictEqual(403);
         expect(result.data).toBeUndefined();
       });
+  // eslint-disable-next-line max-len
+  it('When getProperty and tenantEmails and no administratorEmails then 200 with data',
+      async () => {
+        const propertyModel: Property = {
+          id: 'id',
+          title: 'title',
+          tenantEmails: ['email'],
+          createdByEmail: 'createdByEmail',
+          administratorEmails: [],
+        };
+        const getProperty = makeGetProperty(
+            // @ts-ignore
+            {findOne: () => propertyModel},
+        );
+        const requestingUser = {
+          email: 'email',
+        };
+        // @ts-ignore
+        const result = await getProperty(requestingUser, 'id');
+        expect(result.status).toStrictEqual(200);
+        expect(result.data.id).toStrictEqual('id');
+        expect(result.data.title).toStrictEqual('title');
+        expect(result.data.tenantEmails).toStrictEqual(['email']);
+        expect(result.data.createdByEmail).toStrictEqual('createdByEmail');
+        expect(result.data.administratorEmails).toStrictEqual([]);
+      });
+  // eslint-disable-next-line max-len
+  it('When getProperty and administratorEmails and no tenantEmails then 200 with data',
+      async () => {
+        const propertyModel: Property = {
+          id: 'id',
+          title: 'title',
+          tenantEmails: [],
+          createdByEmail: 'createdByEmail',
+          administratorEmails: ['email'],
+        };
+        const getProperty = makeGetProperty(
+            // @ts-ignore
+            {findOne: () => propertyModel},
+        );
+        const requestingUser = {
+          email: 'email',
+        };
+        // @ts-ignore
+        const result = await getProperty(requestingUser, 'id');
+        expect(result.status).toStrictEqual(200);
+        expect(result.data.id).toStrictEqual('id');
+        expect(result.data.title).toStrictEqual('title');
+        expect(result.data.tenantEmails).toStrictEqual([]);
+        expect(result.data.createdByEmail).toStrictEqual('createdByEmail');
+        expect(result.data.administratorEmails).toStrictEqual(['email']);
+      });
+  // eslint-disable-next-line max-len
+  it('When getProperty and administratorEmails and tenantEmails then 200 with data',
+      async () => {
+        const propertyModel: Property = {
+          id: 'id',
+          title: 'title',
+          tenantEmails: ['email'],
+          createdByEmail: 'createdByEmail',
+          administratorEmails: ['email'],
+        };
+        const getProperty = makeGetProperty(
+            // @ts-ignore
+            {findOne: () => propertyModel},
+        );
+        const requestingUser = {
+          email: 'email',
+        };
+        // @ts-ignore
+        const result = await getProperty(requestingUser, 'id');
+        expect(result.status).toStrictEqual(200);
+        expect(result.data.id).toStrictEqual('id');
+        expect(result.data.title).toStrictEqual('title');
+        expect(result.data.tenantEmails).toStrictEqual(['email']);
+        expect(result.data.createdByEmail).toStrictEqual('createdByEmail');
+        expect(result.data.administratorEmails).toStrictEqual(['email']);
+      });
+  it('When getProperty and throw error Then 500 undefined',
+      async () => {
+        const getProperty = makeGetProperty(
+            // @ts-ignore
+            {
+              findOne: () => {
+                throw Error();
+              },
+            },
+        );
+
+        // @ts-ignore
+        const result = await getProperty({}, 'propertyId');
+        expect(result.status).toEqual(500);
+        expect(result.data).toBeUndefined();
+      });
 });
