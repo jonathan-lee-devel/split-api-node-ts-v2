0d25e627ede0f4e49fa1cf9651e8035599f46399
Jonathan Lee <jonathan.lee.devel@gmail.com>
2022-07-08 18:38:09 +0100

    Added tests to get-property.spec.ts

--- src/properties/services/__tests__/get-property.spec.ts
+++ src/properties/services/__tests__/get-property.spec.ts
@@ -1,10 +1,46 @@
 import {makeGetProperty} from '../get-property';
+import {Property} from '../../models/Property';
 
 describe('Get property', () => {
-  it('When makeGetProperty Then getProperty', async () => {
-    // @ts-ignore
-    const getProperty = makeGetProperty({});
-    expect(getProperty).not.toBeNull();
-    expect(getProperty).toBeInstanceOf(Function);
-  });
+  it('When makeGetProperty Then getProperty',
+      async () => {
+        // @ts-ignore
+        const getProperty = makeGetProperty({});
+        expect(getProperty).not.toBeNull();
+        expect(getProperty).toBeInstanceOf(Function);
+      });
+  it('When getProperty and no property then Return 404 undefined',
+      async () => {
+        const getProperty = makeGetProperty(
+            // @ts-ignore
+            {findOne: () => null},
+        );
+
+        // @ts-ignore
+        const result = await getProperty({}, 'propertyId');
+        expect(result.status).toEqual(404);
+        expect(result.data).toBeUndefined();
+      });
+  // eslint-disable-next-line max-len
+  it('When getProperty and no tenantEmails or administratorEmails then 403 undefined',
+      async () => {
+        const propertyModel: Property = {
+          id: 'id',
+          title: 'title',
+          tenantEmails: [],
+          createdByEmail: 'createdByEmail',
+          administratorEmails: [],
+        };
+        const getProperty = makeGetProperty(
+            // @ts-ignore
+            {findOne: () => propertyModel},
+        );
+        const requestingUser = {
+          email: 'email',
+        };
+        // @ts-ignore
+        const result = await getProperty(requestingUser, 'id');
+        expect(result.status).toStrictEqual(403);
+        expect(result.data).toBeUndefined();
+      });
 });
