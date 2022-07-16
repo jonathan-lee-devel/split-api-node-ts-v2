efa5b2939d64b3dc5695b65d3abcbbba55209b3d
Jonathan Lee <jonathan.lee.devel@gmail.com>
2022-07-16 05:41:12 +0100

    SPLIT-40 Added basic functionality for create notification route

--- src/notifications/routes/index.ts
+++ src/notifications/routes/index.ts
@@ -1,6 +1,14 @@
 import express from 'express';
+import {configureCreateNotificationRoute} from './create-notification';
+import {createNotification} from '../services';
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
+
+configureCreateNotificationRoute(
+    router,
+    '/create',
+    createNotification,
+);
 
 export {router as NotificationsRouter};
