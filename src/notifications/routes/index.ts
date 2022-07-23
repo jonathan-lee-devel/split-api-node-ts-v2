eb5946850ba12b53a30e169b1783c96eb4a78f77
Jonathan Lee <jonathan.lee.devel@gmail.com>
2022-07-23 04:59:04 +0100

    SPLIT-44 Implemented controller pattern for notifications

--- src/notifications/routes/index.ts
+++ src/notifications/routes/index.ts
@@ -1,22 +1,24 @@
 import express from 'express';
-import {configureCreateNotificationRoute} from './create-notification';
-import {createNotification, getNotificationsForUser} from '../services';
 // eslint-disable-next-line max-len
-import {configureGetNotificationsForUserRoute} from './get-notifications-for-user';
+import {loggerConfig} from '../../main/config/logger/logger-config';
+import {configureRoute} from '../../main/routes/configure-route';
+import {HttpRequestMethod} from '../../main/enums/http-request-method';
+// eslint-disable-next-line max-len
+import {makeExpressCallback} from '../../main/express-callbacks/express-callback';
+import {getNotificationsForUserController} from '../controllers';
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-configureCreateNotificationRoute(
-    router,
-    '/create',
-    createNotification,
-);
+const logger = loggerConfig();
 
-configureGetNotificationsForUserRoute(
+configureRoute(
     router,
+    HttpRequestMethod.GET,
     '/',
-    getNotificationsForUser,
+    true,
+    [],
+    makeExpressCallback(logger, getNotificationsForUserController),
 );
 
 export {router as NotificationsRouter};
