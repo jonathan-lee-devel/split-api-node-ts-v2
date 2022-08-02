157b3fb3317d9a2f3386b6bf030e1ec6e427f905
eeejnlo <jonathan.lee@ericsson.com>
2022-08-02 03:09:43 +0100

    SPLIT-51 Implemented unit tests for create expense controller

---
+++ src/expenses/controllers/__tests__/create-expense.spec.ts
@@ -0,0 +1,100 @@
+import {makeCreateExpenseController} from '../create-expense';
+import {HttpRequest} from '../../../main/types/http-request';
+import {ExpenseFrequency} from '../../enums/ExpenseFrequency';
+import {User} from '../../../users/main/models/User';
+
+describe('Create expense controller unit tests', () => {
+  it('When makeCreateExpenseController Then createExpenseController',
+      async () => {
+        const createExpenseController = makeCreateExpenseController(
+            // @ts-ignore
+            () => {
+            },
+        );
+        expect(createExpenseController).not.toBeNull();
+        expect(createExpenseController).toBeInstanceOf(Function);
+      });
+  // eslint-disable-next-line max-len
+  it('When createExpenseController called with HttpRequest Then service called with HttpRequest',
+      async () => {
+        const returnedStatus = 200;
+        const returnedBody = {data: 'test'};
+        let requestedUser: User;
+        let obtainedPropertyId: string;
+        let createdTitle: string;
+        let createdAmount: number;
+        let createdFrequency: ExpenseFrequency;
+        let createdDate: Date;
+        const date = new Date();
+        const createExpenseController = makeCreateExpenseController(
+            // @ts-ignore
+            (
+                requestingUser,
+                propertyId,
+                title,
+                amount,
+                frequency,
+                date,
+            ) => {
+              requestedUser = requestingUser;
+              obtainedPropertyId = propertyId;
+              createdTitle = title;
+              createdAmount = amount;
+              createdFrequency = frequency;
+              createdDate = date;
+              return {
+                status: returnedStatus,
+                data: returnedBody,
+              };
+            },
+        );
+        const httpRequest: HttpRequest = {
+          user: {username: 'test'},
+          body: {
+            propertyId: 'propertyId',
+            title: 'title',
+            amount: -1.00,
+            frequency: ExpenseFrequency.YEARLY,
+            date,
+          },
+          params: undefined,
+        };
+        await createExpenseController(httpRequest);
+        expect(requestedUser).toStrictEqual({username: 'test'});
+        expect(obtainedPropertyId).toStrictEqual('propertyId');
+        expect(createdTitle).toStrictEqual('title');
+        expect(createdAmount).toStrictEqual(-1.00);
+        expect(createdFrequency).toStrictEqual(ExpenseFrequency.YEARLY);
+        expect(createdDate).toStrictEqual(date);
+      });
+  // eslint-disable-next-line max-len
+  it('When createExpenseController called with HttpRequest and service returns status and body Then service called with HttpRequest returns status and body',
+      async () => {
+        const returnedStatus = 200;
+        const returnedBody = {data: 'test'};
+        const createExpenseController = makeCreateExpenseController(
+            // @ts-ignore
+            () => {
+              return {
+                status: returnedStatus,
+                data: returnedBody,
+              };
+            },
+        );
+        const httpRequest: HttpRequest = {
+          user: {username: 'test'},
+          body: {
+            propertyId: 'propertyId',
+            title: 'title',
+            amount: -1.00,
+            frequency: ExpenseFrequency.YEARLY,
+            date: new Date(),
+          },
+          params: undefined,
+        };
+        const httpControllerResult =
+                await createExpenseController(httpRequest);
+        expect(httpControllerResult.httpStatus).toStrictEqual(returnedStatus);
+        expect(httpControllerResult.jsonBody).toStrictEqual(returnedBody);
+      });
+});
