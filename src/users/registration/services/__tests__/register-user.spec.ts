2d4b04e81b0c0e8917d9b1e00a0cee24b974fea3
Jonathan Lee <43010048+jonathan-lee-devel@users.noreply.github.com>
2023-03-24 23:01:40 +0000

    SPLIT-69 Implemented unit tests for users/registration services (#32)

---
+++ src/users/registration/services/__tests__/register-user.spec.ts
@@ -0,0 +1,115 @@
+import {makeRegisterUser} from '../register-user';
+import {RegistrationStatus} from '../../enums/RegistrationStatus';
+
+describe('Registration Service Register User Tests', () => {
+  it('When makeRegisterUser Then registerUser', async () => {
+    // @ts-ignore
+    const registerUser = makeRegisterUser(
+        {},
+        () => {
+        },
+        () => {
+        },
+        () => {
+        },
+        () => {
+        },
+        {},
+    );
+
+    expect(registerUser).not.toBeNull();
+    expect(registerUser).toBeInstanceOf(Function);
+  });
+  it('When handleExistingUser returns true Then return conflict status', async () => {
+    // @ts-ignore
+    const registerUser = makeRegisterUser(
+        {},
+        () => {
+          return true;
+        },
+        () => {
+        },
+        () => {
+        },
+        () => {
+        },
+        {},
+    );
+
+    const result = await registerUser('test@mail.com', 'John', 'Doe', 'password');
+
+    expect(result.status).toStrictEqual(409);
+    expect(result.data).toStrictEqual({
+      status: RegistrationStatus[RegistrationStatus.USER_ALREADY_EXISTS],
+    });
+  });
+  it('When handleExistingUser returns false and password token fails Then return failure status', async () => {
+    // @ts-ignore
+    const registerUser = makeRegisterUser(
+        {},
+        () => {
+          return false;
+        },
+        () => {
+          return {
+            status: 201,
+          };
+        },
+        () => {
+          return {
+            status: 500,
+          };
+        },
+        () => {
+        },
+        {},
+    );
+
+    const result = await registerUser('test@mail.com', 'John', 'Doe', 'password');
+
+    expect(result.status).toStrictEqual(500);
+    expect(result.data).toStrictEqual({
+      status: RegistrationStatus[RegistrationStatus.FAILURE],
+    });
+  });
+  it('When tokens generated and user saved Then return status 200 and awaiting approval', async () => {
+    const UserModel = function() {
+      this.save = () => {
+      };
+    };
+    const registerUser = makeRegisterUser(
+        // @ts-ignore
+        {},
+        () => {
+          return false;
+        },
+        () => {
+          return {
+            status: 201,
+            data: {
+              value: '1234',
+            },
+          };
+        },
+        () => {
+          return {
+            status: 201,
+          };
+        },
+        () => {
+        },
+        UserModel,
+        () => {
+        },
+    );
+    const result = await registerUser(
+        'user@mail.com',
+        'John',
+        'Doe',
+        'password',
+    );
+
+    expect(result.status).toStrictEqual(200);
+    expect(result.data.status).toStrictEqual(RegistrationStatus[RegistrationStatus.AWAITING_EMAIL_VERIFICATION]);
+  });
+});
