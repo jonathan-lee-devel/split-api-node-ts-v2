a7126f8837de6b630b7a77548d3b7a6c75cc7461
Jonathan Lee <jonathan.lee.devel@gmail.com>
2023-03-25 01:35:12 +0000

    Updated jest.config.cjs

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,11 +1,11 @@
 module.exports = {
-  preset: 'ts-jest/presets/default-esm',
-  globals: {
-    "ts-jest": {
-      "useESM": true
-    }
-  },
-  testEnvironment: 'node',
-  collectCoverage: true,
-  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
+    preset: 'ts-jest/presets/default-esm',
+    globals: {
+        "ts-jest": {
+            "useESM": true
+        }
+    },
+    testEnvironment: 'node',
+    collectCoverage: true,
+    testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)', '**/?index.ts?(x)'],
 };
